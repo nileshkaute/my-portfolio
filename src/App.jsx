944d5b1e77a993b8e948364c1e4513109d92b4fb
Nilesh Kaute <nileshkaute1@gmail.com>
2025-12-09 23:01:41 +0530

    added some css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ReactLenis, useLenis } from 'lenis/react'
 
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
-// import Footer from './components/Footer.jsx';
+
 
 
 const App = () => {
@@ -29,7 +29,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
-      {/* <Footer/> */}
+     
     </>
   );
 };
